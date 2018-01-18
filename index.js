609e90ae83ad818b457247532d876fb3bf18aeb7
Sebastian Schlegel <sebastianschlegel1@gmx.net>
2018-01-18 11:17:19 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,9 @@
+function BeatlesPlay(musicians,instruments){
+  var array = [];
+  
+  for(let i = 0;i < musicians.length;i++){
+    array.push("$musicians[i] play $instruments[i]")
+  }
+  
+  return array;
+}
