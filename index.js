5e09c01031aca317c4c0b8b8b6263bddd4e87ebd
Sebastian Schlegel <sebastianschlegel1@gmx.net>
2018-01-18 11:31:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function theBeatlesPlay(musicians,instruments){
 function johnLennonFacts(facts){
   var i;
   while(facts.lengt <= i){
-    facts[0] = facts[0] + "!!!";
+    facts[i] = facts[i] + "!!!";
     i++;
   }
   return facts;
