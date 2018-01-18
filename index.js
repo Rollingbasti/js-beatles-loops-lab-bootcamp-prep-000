b4368dcdb03c0a3e78b19635da318cd34362ce54
Sebastian Schlegel <sebastianschlegel1@gmx.net>
2018-01-18 11:23:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function theBeatlesPlay(musicians,instruments){
   var array = [];
   
   for(let i = 0;i < musicians.length;i++){
-    array.push("musicians[i] play $instruments[i]")
+    array.push(musicians[i] + " play " + instruments[i]);
   }
   
   return array;
